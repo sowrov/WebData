af79e8db0d93f9ae6ffa872f4b1690e2a3663fab
sowrov <sowrov@gmail.com>
2020-08-04 11:26:38 +0200

    Initial project

---
+++ src/main/java/com/mozadded/WebData/WebDataApplication.java
@@ -0,0 +1,13 @@
+package com.mozadded.WebData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mozadded/WebData/WebDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mozadded.WebData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
